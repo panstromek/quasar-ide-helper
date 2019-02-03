f459d715f51cca61b98d8eedabdb482965c9647d
panstromek <panstromek@seznam.cz>
2019-02-03 22:41:52 +0100

    create collect APIs helper

--- extension/collectApis.js
+++ extension/collectApis.js
@@ -1,18 +1,13 @@
 /**
  * @param {String} dir
- * @return {Array}
+ * @return {[{name:string, api:Object}]} Array of APis
  */
 module.exports = function (dir) {
-  const apiPath = `${dir}/node_modules/quasar/dist/api`
-  const apis = fs.readdirSync(apiPath)
-  return apis.map(name => {
-    const api = require(`${apiPath}/${name}`)
-    if (name.endsWith('.json')) {
-      name = name.substring(0, name.length - 5)
-    }
-    return {
-      name,
-      api
-    }
-  })
+  return fs
+    .readdirSync(`${dir}/node_modules/quasar/dist/api`)
+    .filter(name => name.endsWith('.json'))
+    .map(name => ({
+      name: name.substring(0, name.length - 5),
+      api: require(`${`${dir}/node_modules/quasar/dist/api`}/${name}`)
+    }))
 }
