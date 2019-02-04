73e239479a163ebbbe40d8c556bfd4d6fefb4ac5
panstromek <panstromek@seznam.cz>
2019-02-04 20:50:02 +0100

    refactor

--- extension/type-generator.js
+++ extension/type-generator.js
@@ -53,123 +53,122 @@ module.exports = function (appDir) {
     `
   }`)
 
-  function directive (name, api) {
-    return `
-Vue.directive('${name}', {})
-`
-  }
+}
 
-  function paramDoc (params) {
-    if (!params) {
-      return ``
-    }
-    return Object.entries(params).map(([name, param]) => {
-      const paramName = param.required ? name : `[${name}]`
-      return `
+function paramDoc (params) {
+  if (!params) {
+    return ``
+  }
+  return Object.entries(params).map(([name, param]) => {
+    const paramName = param.required ? name : `[${name}]`
+    return `
     * @param {${typeComment(param.type)}} ${paramName} ${param.desc}`
-    }).reduce((params, param) => params + param, ``)
-  }
+  }).reduce((params, param) => params + param, ``)
+}
 
-  function returns (returns) {
-    if (!returns) {
-      return ``
-    }
-    return `
+function returns (returns) {
+  if (!returns) {
+    return ``
+  }
+  return `
     * @return {${typeComment(returns.type)}} ${returns.desc}`
-  }
+}
 
-  function pluginMethods (methods) {
-    if (!methods) {
-      return ``
-    }
-    return Object.entries(methods).map(([name, methodApi]) => {
-      return `
+function pluginMethods (methods) {
+  if (!methods) {
+    return ``
+  }
+  return Object.entries(methods).map(([name, methodApi]) => {
+    return `
    /**
     * ${methodApi.desc}${paramDoc(methodApi.params)}${returns(methodApi.returns)}
     */
     ${name} (${Object.keys(methodApi.params || {})}) {}`
-    })
-  }
+  })
+}
 
-  function plugin (name, api) {
-    if (api.injection === undefined) {
-      // TODO meta plugin
-      console.log('TODO', name, api)
-      return ``
-    }
-    return `
+function plugin (name, api) {
+  if (api.injection === undefined) {
+    // TODO meta plugin
+    return ``
+  }
+  return `
 ${api.injection.substring(3)} : {
   ${objectProps(api.props)}
   ${pluginMethods(api.methods)}
 },
 `
-  }
+}
 
-  function component (name, api) {
-    return `
+function component (name, api) {
+  return `
 Vue.component('${name}', {
   name: '${name}',
   props: {${vueProps(api.props)}
   }
 })
 `
-  }
+}
 
-  function typeComment (type) {
-    if (Array.isArray(type)) {
-      type = type.toString().replace(/,/g, '|')
-    }
-    return type.replace(/Any/g, '*')
+function typeComment (type) {
+  if (Array.isArray(type)) {
+    type = type.toString().replace(/,/g, '|')
   }
+  return type.replace(/Any/g, '*')
+}
 
-  function generateVueType (type) {
-    if (Array.isArray(type)) {
-      return `[${type.toString()}]`
-    }
-    return type
+function generateVueType (type) {
+  if (Array.isArray(type)) {
+    return `[${type.toString()}]`
   }
+  return type
+}
 
-  function propComment (prop) {
-    return `
+function propComment (prop) {
+  return `
     /**
      * ${prop.desc}${prop.reactive ? ' (reactive)' : ''}
      * @type {${typeComment(prop.type)}}
      */`
-  }
+}
 
-  function vueType (type) {
-    if (type === 'Any' || (Array.isArray(type) && type.includes('Any'))) {
-      return ``
-    }
-    return `
+function vueType (type) {
+  if (type === 'Any' || (Array.isArray(type) && type.includes('Any'))) {
+    return ``
+  }
+  return `
       type: ${generateVueType(type)},`
-  }
+}
 
-  function objectProps (props) {
-    if (!props) {
-      return ``
-    }
-    return Object.entries(props)
-      .map(([name, prop]) => {
-        return `${propComment(prop)}
+function objectProps (props) {
+  if (!props) {
+    return ``
+  }
+  return Object.entries(props)
+    .map(([name, prop]) => {
+      return `${propComment(prop)}
     '${name}': {}`
-      }).toString() + ','
-  }
+    }).toString() + ','
+}
 
-  function vueProps (props) {
-    if (!props) {
-      return ``
-    }
-    return Object.entries(props)
-      .map(([name, prop]) => {
-        const VueType = vueType(prop.type)
-        const required = prop.required ? `
+function vueProps (props) {
+  if (!props) {
+    return ``
+  }
+  return Object.entries(props)
+    .map(([name, prop]) => {
+      const VueType = vueType(prop.type)
+      const required = prop.required ? `
       required: true` : ``
 
-        return `${propComment(prop)}
+      return `${propComment(prop)}
     ${toCamel(name)}: {${VueType}${required}
     }`
-      })
-  }
+    })
+}
 
+function directive (name, api) {
+  return `
+Vue.directive('${name}', {})
+`
 }
