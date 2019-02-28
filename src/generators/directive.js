dde0fbc8f02f904cfd01dee8d9578e59eb8311b5
panstromek <panstromek@seznam.cz>
2019-03-01 00:59:05 +0100

    add basic comments to directives

--- src/generators/directive.js
+++ src/generators/directive.js
@@ -1,11 +1,20 @@
+const { typeComment } = require('../utils/comments')
+
 function functionDirective (name, value) {
-  return `Vue.directive('${name}="(${Object.keys(value.params).map(p => p.toLowerCase()).join(', ')}) => {}"', {})\n`
+  return `
+/*
+ * @param {${typeComment(value)}} ${value && value.desc}
+ */
+Vue.directive('${name}="(${Object.keys(value.params).map(p => p.toLowerCase()).join(', ')}) => {}"', {})\n`
 }
 
 module.exports = {
   directive (name, { arg = {}, value = {}, modifiers = {} }) {
-    return `Vue.directive('${name}', {})\n`
-      + (value.type === 'Function' ?
-        functionDirective(name, value) : '')
+    return `
+/*
+ * @param {${typeComment(value)}} ${value && value.desc}
+ */
+Vue.directive('${name}', {})\n${value.type === 'Function' ?
+      functionDirective(name, value) : ''}`
   }
 }
