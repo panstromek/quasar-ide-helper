eddf8a08e051b9971d0885d886ad1c435dced184
panstromek <panstromek@seznam.cz>
2019-02-28 23:01:39 +0100

    fix boolean in enums

--- src/utils/comments.js
+++ src/utils/comments.js
@@ -1,7 +1,16 @@
 const me = module.exports = {
   typeComment ({ type, desc, definition, values, examples }) {
     if (values) {
-      return values.map(val => typeof val === 'string' ? `'${val}'` : val).join('|')
+      return values.map(val => {
+        switch (typeof val) {
+          case 'string':
+            return `'${val}'`
+          case 'boolean':
+            return `Boolean`
+          default :
+            return val
+        }
+      }).filter((t, i, arr) => arr.lastIndexOf(t) === i).join('|')
     }
     if (Array.isArray(type)) {
       type = type.toString().replace(/,/g, '|')
